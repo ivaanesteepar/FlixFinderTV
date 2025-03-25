7342c444a4e602d2c3e77ba6112091709493490c
Ivan <ivanestepar999@gmail.com>
2025-03-26 00:27:17 +0100

    He eliminado el analisis de coverage ya que en principio no voy a realizar pruebas unitarias

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ sonarqube {
         property("sonar.projectKey", "ivaanesteepar_FlixFinderTV")
         property("sonar.organization", "ivaanesteepar")
         property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.exclusions", "**/test/**")
     }
 }
 
