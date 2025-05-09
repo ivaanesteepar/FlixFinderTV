7876ca665dbf2b63a16c891c1c5746a5d7aab766
Ivan <ivanestepar999@gmail.com>
2025-05-09 12:38:47 +0200

    He borrado la caché de git para que no me rastree archivos especificos de configuracion

--- app/src/main/java/com/example/flixfindertv/models/Generos.kt
+++
@@ -1,9 +0,0 @@
-package com.example.flixfindertv.models
-
-// Clase que representa un género con su identificador y nombre
-data class Generos (
-    val id: Int,
-    val name: String
-){
-    constructor() : this(0, "")
-}
