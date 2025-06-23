40b805c6a174e18e0cb0e5543481dff18c9c22e0
Ivan <ivanestepar999@gmail.com>
2025-06-23 18:51:16 +0200

    Cambios mínimos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.ivaanesteepar.flixfindertv"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         // Cargar el archivo local.properties
         val localProperties = Properties()
